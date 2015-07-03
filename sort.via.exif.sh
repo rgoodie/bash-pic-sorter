66b36dc43afbc122974c7ac296d04479b42066d1
richard <r.chardgoodrow@gmail.com>
2015-07-03 14:38:42 -0500

    Adding MOVs Mod Date/Time

--- sort.via.exif.sh
+++ sort.via.exif.sh
@@ -22,4 +22,24 @@ do
 	
 done;
 
+# For anything left over, use the Modficaiton Date. Include MOVs in this
+# round. They didn't have the Original Date when I was testing. 
+for FILE in `find -type f | egrep "(png$|jpg$|mov$|JPG$|PNG$|MOV$)"`; 
+do 
+	RAW=`exiftool $FILE | exiftool *.MOV | grep "File Modification Date/Time"`
+	YEAR=`echo $RAW | perl -pe 's/.*([0-9]{4}).*/\1/g'`
+	MONT=`echo $RAW | perl -pe 's/.*[0-9]{4}:([0-9]{2}).*/\1/g'`
+	FOLDER="./$YEAR/$MONT"
+
+	# make folder if not exists
+	[ -d $FOLDER ] || mkdir -p $FOLDER
+
+	# move file to that folder but only if there is data
+	# to provide a year/month folder option
+	if [ -n "$YEAR" ]; then
+		mv $FILE $FOLDER
+	fi
+
+done;
+		
 # preg_match('/([0-9]{4})/',$sourcestring,$matches)
