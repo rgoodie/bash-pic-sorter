e5c39ca633a706d045ed1af149f8ac633a08b0b7
richard <r.chardgoodrow@gmail.com>
2016-01-01 09:50:14 -0600

    This one file does the trick. Others unneeded. Removed old code.

--- makeyear.sh
+++
@@ -1,9 +0,0 @@
-for YEAR in `seq 2012 2015`; 
-do 
-	
-	for MONTH in `seq 1 12`;
-	do 
-		mkdir -p $YEAR/$MONTH/;
-	done;
-	
-done;
--- sort.via.exif.sh
+++ sort.via.exif.sh
@@ -1,32 +1,13 @@
-# Attemts to 
 
-for FILE in `find -type f | egrep "(png$|jpg$|JPG$|PNG$)"`; 
-do 
-	
-	# Get Year and Month inside a variable to 
-	# use later in processing
-	RAW=`exiftool -DateTimeOriginal $FILE`
-	YEAR=`echo $RAW | perl -pe 's/.*([0-9]{4}).*/\1/g'`
-	MONT=`echo $RAW | perl -pe 's/.*[0-9]{4}:([0-9]{2}).*/\1/g'`
-	FOLDER="./$YEAR/$MONT"
-
-	# make folder if not exists
-	[ -d $FOLDER ] || mkdir -p $FOLDER
-
-	# move file to that folder but only if there is data
-	# to provide a year/month folder option
-	if [ -n "$YEAR" ]; then
-		mv $FILE $FOLDER
-	fi
-		
-	
-done;
 
 # For anything left over, use the Modficaiton Date. Include MOVs in this
 # round. They didn't have the Original Date when I was testing. 
 for FILE in `find -type f | egrep "(png$|jpg$|mov$|JPG$|PNG$|MOV$)"`; 
 do 
-	RAW=`exiftool $FILE | exiftool *.MOV | grep "File Modification Date/Time"`
+
+	echo $FILE
+
+	RAW=`exiftool $FILE | grep "File Modification Date/Time"`
 	YEAR=`echo $RAW | perl -pe 's/.*([0-9]{4}).*/\1/g'`
 	MONT=`echo $RAW | perl -pe 's/.*[0-9]{4}:([0-9]{2}).*/\1/g'`
 	FOLDER="./$YEAR/$MONT"
@@ -42,4 +23,3 @@ do
 
 done;
 		
-# preg_match('/([0-9]{4})/',$sourcestring,$matches)
